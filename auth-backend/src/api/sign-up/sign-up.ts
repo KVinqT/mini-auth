53e0ff140c380ca0955ebb0ac886e98fe7a3f596
KVinqT <kevinqt4you@gmail.com>
2024-12-01 00:37:48 +0630

    has password with bcrypt and salt 13

--- auth-backend/src/api/sign-up/sign-up.ts
+++ auth-backend/src/api/sign-up/sign-up.ts
@@ -1,8 +1,12 @@
 import pg from "@config/pg-pool";
 import { NextFunction, Request } from "express";
+import bcrypt from "bcrypt";
+
 const signup = async (req: Request, res: any, next: NextFunction) => {
   try {
     const userCredentials = req.body;
+    const hasPassword = await bcrypt.hash(userCredentials.password, 13);
+    userCredentials.password = hasPassword;
     const queryText = `INSERT INTO "user" (user_name,email,password) VALUES ($1,$2,$3) RETURNING *`;
     const values = [
       userCredentials.username,
@@ -10,8 +14,9 @@ const signup = async (req: Request, res: any, next: NextFunction) => {
       userCredentials.password,
     ];
     const result = await pg.query(queryText, values);
-    console.log("Added Data", result.rows);
-    return res.status(200).json("User data added successfully");
+    return res
+      .status(200)
+      .json(`User ${result.rows[0].user_name} added successfully`);
   } catch (error) {
     next(error);
   }
